718eeaf4d7dd0d6a96179e4a385408d68ba1b190
Balram <singhbalram054@gmail.com>
2019-06-12 17:04:05 +0530

    Basic comcepts

--- check.cpp
+++ check.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"Hello World Check";
+	//char c=65;
+	//c=c+2;
+	//cout<<"Value of char "<<c+2<<endl;
+	//cout<<"Value of boolen "<<c<<endl;
+
+	//char c=65;
+	//bool b = 65;
+	//c= c+b;
+	//cout<<c+b<<endl; // because of + operator this value give  integer form
+	//cout<<c;
+
+	//int x =5;
+	//cout<<(x==6);
+
+
+	//int c=65;
+	//char b = 65;
+	//c= c+b;
+	//cout<<c+b<<endl; // because of + operator this value give  integer form
+	//cout<<c;
+
+	float c=65.50;
+	char b = 65;
+	c= c+b;
+	cout<<c+b<<endl; // because of + operator this value give  integer form
+	cout<<c;
+
+
+
 	return 0;
 }
