a4d13a1bf00bcf6381f03ba3efc3bcb4493e8a06
Balram <singhbalram054@gmail.com>
2019-06-14 01:15:42 +0530

    some basic concpts

--- ponter/pointerbasic.cpp
+++ ponter/pointerbasic.cpp
@@ -3,10 +3,24 @@ using namespace std;
 
 int main(){
 
-        int x= 10;
-        cout<<&x;
+        // int x= 10;
+        // float y=10.5;
+        // cout<<&x<<endl;
+        // cout<<&y;
 
 
+        //poiner is variable that store address of another variable
+        // if u want to stpore the variable than pointer is created
 
+
+    // int x = 10;
+    // int*ptr = &x;
+    // cout<<x<<endl;
+    // cout<<&x<<endl;
+    // cout<<ptr<<endl;
+    // cout<<*ptr<<endl;
+
+    //ptr=&x
+        
     return 0;
 }
