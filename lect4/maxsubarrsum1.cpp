1678057ad1da27f1b3ad1cc4c48397b8c696b0de
Balram <singhbalram054@gmail.com>
2019-06-19 11:59:13 +0530

    max sum arr

---
+++ lect4/maxsubarrsum1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[]){
+
+	int n;
+	cin>>n;
+	int maxSum=0;
+	int curSum=0;
+	int arr[100];
+	for (int i = 0; i < n; i++){
+		cin>>arr[i];
+
+	}
+
+	for (int i = 0; i < n; i++){
+		for (int j = i; j < n; j++){
+			int curSum=0;
+			for (int k = i; k <= j; k++){
+				curSum=curSum+arr[k];
+			}
+			if (maxSum<curSum)
+			{
+				maxSum=curSum;
+			}
+			
+		}
+
+
+	}
+		cout<<"maximum sum is "<<maxSum;
+	
+	return 0;
+}
+
+
