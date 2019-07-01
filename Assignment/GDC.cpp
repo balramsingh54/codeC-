175d7431993ee7fa6705215491c89a50419611f2
Balram <singhbalram054@gmail.com>
2019-07-01 11:57:06 +0530

    p

--- Assignment/GDC.cpp
+++ Assignment/GDC.cpp
@@ -1,9 +1,34 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(int argc, char const *argv[])
-{
-	
+#include <bits/stdc++.h> 
+using namespace std; 
+  
 
-	
-	return 0;
+int gcd(int a, int b) 
+{ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+
+int findGCD(int arr[], int n) 
+{ 
+    int result = arr[0]; 
+    for (int i = 1; i < n; i++) 
+        result = gcd(arr[i], result); 
+  
+    return result; 
+} 
+  
+// Driven code 
+int main() 
+{ 
+	int size;
+	cin>>size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+    	cin>>arr[i];
+    }
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    cout << findGCD(arr, n) << endl; 
+    return 0; 
 }
